dc4afe1535d7ec287b68434bf0ab1071fd5509c1
Jay Hung <37239758+JayChung0258@users.noreply.github.com>
2025-03-31 16:37:54 -0700

    first init

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Outfit, Ovo } from "next/font/google";
+import "./globals.css";
+
+const outfitFont = Outfit({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
+const ovoFont = Ovo({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+export const metadata: Metadata = {
+  title: "Portfolio",
+  description: "Portfolio of Jay Hung",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${outfitFont.className} ${ovoFont.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
