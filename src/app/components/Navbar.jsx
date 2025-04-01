905cca8d24e1ddb63bf4b4db651a106acfe49015
Jay Hung <37239758+JayChung0258@users.noreply.github.com>
2025-04-01 16:28:52 -0700

    feature: add dynamic rendering

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,12 +1,13 @@
 'use client';
 
-import React, { useRef } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import Image from 'next/image'
 import { assets } from '../assets/assets'
 
 
 const Navbar = () => {
 
+    const [isScrolled, setIsScrolled] = useState(false);
     const sideMenuRef = useRef();
 
     const openMenu = () => {
@@ -16,19 +17,30 @@ const Navbar = () => {
         sideMenuRef.current.style.transform = 'translateX(16em)';
     }
 
+    useEffect(() => {
+        window.addEventListener('scroll', () => {   
+            if (window.scrollY > 50) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        });
+    }, []);
+        
     return (
         <>
             <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
                 <Image src={assets.header_bg_color} alt="" className='w-full' />
             </div>
 
-            <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
+            <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 
+                ${isScrolled ? 'bg-white bg-opacity-50 backdrop-blur-lg shadow-sm transition duration-500' : 'transition duration-500'}`}>
                 <a href="">
                     <Image src={assets.logo} alt="" className='w-28 cursor-pointer mr-14' />
                 </a>
 
-                <ul className='hidden md:flex items-center gap-6 lg:gap-8 
-                            rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50'>
+                <ul className={`hidden md:flex items-center gap-6 lg:gap-8 
+                            rounded-full px-12 py-3 ${isScrolled ? '' : 'bg-white shadow-sm bg-opacity-50 transition duration-500'}`}>
                     <li><a className='font-Ovo' href='#top'>Home</a></li>
                     <li><a className='font-Ovo' href='#about'>About</a></li>
                     <li><a className='font-Ovo' href='#services'>Services</a></li>
